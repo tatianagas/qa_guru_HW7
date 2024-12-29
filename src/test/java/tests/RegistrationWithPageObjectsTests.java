82dacbe67254d301e1ab9844fb2623ce78d93d2a
Tatiana Gasilina <ernolova@gmail.com>
2024-12-29 14:03:54 +0300

    add allure

--- src/test/java/tests/RegistrationWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -1,8 +1,11 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
+@Tag("demoqa")
 public class RegistrationWithPageObjectsTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
